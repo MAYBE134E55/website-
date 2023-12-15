dc1efd53253f818ba33bb47d239a0ddfa38fe28e
MAYBE134E55 <131408128+MAYBE134E55@users.noreply.github.com>
2023-12-15 21:52:46 +0200

    Initial commit
    Initial commit

---
+++ dashboard.php.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["username"])) {
+    header("Location: login.html"); // قم بتوجيه المستخدم إلى صفحة تسجيل الدخول إذا لم يكن مسجل الدخول
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <h2>مرحبًا <?php echo $_SESSION["username"]; ?> في لوحة التحكم</h2>
+    <p>هذه هي الصفحة المحمية.</p>
+
+    <!-- ... المحتوى الإضافي للوحة التحكم ... -->
+
+    <a href="logout.php">تسجيل الخروج</a>
+
+</body>
+</html>
