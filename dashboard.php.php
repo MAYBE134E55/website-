6801a65a70deb1220e1fde3aa56a68bd18174b79
MAYBE134E55 <131408128+MAYBE134E55@users.noreply.github.com>
2023-12-15 22:00:38 +0200

    Update dashboard.php.php

--- dashboard.php.php
+++ dashboard.php.php
@@ -25,3 +25,31 @@ if (!isset($_SESSION["username"])) {
 
 </body>
 </html>
+<?php
+session_start();
+
+if (!isset($_SESSION["email"])) {
+    header("Location: login.html"); // قم بتوجيه المستخدم إلى صفحة تسجيل الدخول إذا لم يكن مسجل الدخول
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>مرحبا بك في موقع ريو وفهد</title>
+</head>
+<body>
+
+    <h2>مرحبًا <?php echo $_SESSION["username"]; ?> في موقع ريو وفهد</h2>
+
+    <p>هذه هي الصفحة المحمية.</p>
+
+    <!-- ... المحتوى الإضافي للوحة التحكم ... -->
+
+    <a href="logout.php">تسجيل الخروج</a>
+
+</body>
+    </html>
